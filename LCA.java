73adf7e9c5dad2e9f3488f0bd7e7312bee5d172d
saumya bahuguna <bahugunas@tcd.ie>
2021-10-07 00:16:37 +0100

    An update to the code at 00:16:37 on 07/10/2021

--- LCA.java
+++ LCA.java
@@ -35,7 +35,7 @@ static Node r = null;
         Node prev, cur;
         prev=null;
         cur=r;
-        while(cur!=null)            /* traverse until correct position is found*/
+        while(cur!=null)            
         {
             prev=cur;
             if (item==cur.d && item!=0){
@@ -75,32 +75,27 @@ static Node r = null;
 }
     public static Node LCA(int x, int y)
     {
-        // return if the tree is empty or `x` or `y` is not present
-        // in the tree
+       
         if (r == null || !Search(x) || !Search(y)) {
             return null;
         }
  
-        // start from the root node
+
         Node curr = r;
  
-        // traverse the tree
+      
         while (curr != null)
         {
-            // if both `x` and `y` is smaller than the root, LCA exists in the
-            // left subtree
+        
             if (curr.d > Integer.max(x, y)) {
                 curr = curr.l;
             }
  
-            // if both `x` and `y` are greater than the root, LCA exists in the
-            // right subtree
+           
             else if (curr.d < Integer.min(x, y)) {
                 curr = curr.ri;
             }
  
-            // if one key is greater (or equal) than the root and one key is
-            // smaller (or equal) than the root, then the current node is LCA
             else {
                 return curr;
             }
@@ -112,7 +107,7 @@ static Node r = null;
         int x,c;
         Node temp=new Node(item);
 
-        //Node y;
+        
         if (item>0)
         {
             if (r == null)
@@ -124,16 +119,22 @@ static Node r = null;
         
             Node v=Insert(x);
              if(v!=null){
-                  c=x;
+                  
                 Create(x);
              }
              else{
-                 
+                  Create(item);
              }
             System.out.println("Enter next item after "+item+" ");
             x=sc.nextInt();
-            Insert(x);
-            Create(x);
+            v=Insert(x);
+            if(v!=null){
+                  
+                Create(x);
+             }
+             else{
+                  Create(item);
+             }
             return null;
             
         }
@@ -155,16 +156,17 @@ static Node r = null;
        
         System.out.println("Creating the Binary search tree , enter appropriate values: ");
         item =sc.nextInt();
-        //Insert(item);
         Create(item);
         int a,b;
          System.out.println(" Enter 0 to EXIT!!");
-           
-      
+           int t= sc.nextInt();
+         while(t!=0){ 
+            System.out.println(" Enter 0 to EXIT!!");
+            t=sc.nextInt();
          System.out.println("Enter the nodes to be checked");
         
          a=sc.nextInt();  
-         while(a!=0){   
+           
         b=sc.nextInt();
         Node lca =LCA(a,b);
         if(lca!=null){
@@ -174,89 +176,13 @@ static Node r = null;
         else {
             System.out.println("No Lowest ancestor found");
         }
-    }
+        
+    }return;
+    
     
 
-
-    }
-/*
-
-    static class NW
-        {
-            public Node n;
-            NW(Node n){
-            this.n=n;
-            }
-        
-
- public static boolean Nodecheck( Node r, Node n)
- {
-     if(r==null)
-     {
-     return false;
-     }
-    
- else if( r ==n) {
-     return true;
-}
-    return Nodecheck(r.l , n) || Nodecheck(r.ri,n);
-
-}
-
-public static boolean FindLCA( Node r, NW lca, Node x, Node y)
-{
-    if(r==null){
-        return false;
     }
 
-        
-        if (r == x || r == y)
-        {
-            
-            lca.n = r;
-            return true;
-        }
- 
-       
-        boolean l = FindLCA(r.l, lca, x, y);
- 
-       
-        boolean ri = FindLCA(r.ri, lca, x, y);
- 
-        
-        if (l && ri) {
-            lca.n = r;
-        }
- 
-       
-        return l || ri;
-    }
- 
-    public static void FindLCA(Node r, Node x, Node y)
-    {
-        
-        Node lca = null;
- 
-       
-        
-        NW LCA = new NW(lca);
- 
-        if (Nodecheck(r, y) && Nodecheck(r, x))
-        {
-            FindLCA(r, LCA, x, y);
-            lca = LCA.n;
-        }
- 
-        
-        if (lca != null) {
-            System.out.println("LCA is " + lca.d);
-        }
-        else {
-            System.out.print("LCA does not exist\n");
-        }
-    }
-
-*/
 public static void main(String[] args)
 {
 
@@ -264,28 +190,6 @@ Main m = new Main();
 
 m.Createtree();
 
-
-
-
-    /*
-
-    Node r = new Node(55);
-    r.l = new Node(22);
-    r.ri = new Node(66);
-    r.l.ri = new Node(44);
-    r.ri.l = new Node(65);
-    r.ri.ri = new Node(68);
-    r.ri.l.l = new Node(60);
-    r.ri.ri.ri = new Node(70);
-
-    FindLCA(r, r.ri.l.l, r.ri.ri);
-    FindLCA(r, r.ri.l.l, new Node(100));
-    FindLCA(r, r.ri.l.l, r.ri.l.l);
-    FindLCA(r, r.ri.l.l, r.ri.l);
-    FindLCA(r, r.l, r.ri.l);
-}
-// this is just to test the auto Commit feature
-*/
 }
 
 }
