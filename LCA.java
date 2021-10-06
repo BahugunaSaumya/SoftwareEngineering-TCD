1a9fb1246eaa89ea7a235ea835212848035a8500
saumya bahuguna <bahugunas@tcd.ie>
2021-10-07 00:46:37 +0100

    An update to the code at 00:46:37 on 07/10/2021

--- LCA.java
+++ LCA.java
@@ -3,9 +3,7 @@
 import java.util.*;
 
 
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-
-//import org.junit.Test;
+import org.junit.Test;
 
 class Node{
 
@@ -191,5 +189,28 @@ m.Createtree();
 
 }
 
+
+
+@Test
+public void testLCA(){
+
+Main m = new Main();
+m.Create(10);
+m.Create(10);
+m.Create(10);
+m.Create(10);
+m.Create(10);
+m.Create(10);
+m.Create(10);
+m.Create(10);
+m.Create(10);
+m.Create(10);
+m.Create(10);
+
+Node r;
+r=LCA(10,0);
+assertEquals(0,r.d);
 }
+}
+
 
